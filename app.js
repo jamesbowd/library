61d8d55cdc23df44630b953fac0c9470d739cec5
James Bowden <jamesbowd@gmail.com>
2020-10-03 21:51:10 +0100

    created functions to display books in a table

---
+++ app.js
@@ -0,0 +1,46 @@
+let myLibrary = [];
+
+const table = document.querySelector("table");
+const form = document.querySelector("form");
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToTable(title, author, pages, read) {
+  const row = document.createElement("tr");
+
+  if (read === true) {
+    read = "Read";
+  } else {
+    read = "Not read";
+  }
+
+  row.innerHTML = `
+  <td>${title}</td>
+  <td>${author}</td>
+  <td>${pages}</td>
+  <td>${read}</td>
+  <td>Toggle</td>
+  <td>Trash</td>
+  `;
+  table.appendChild(row);
+}
+
+function addLibrarytoTable(arr) {
+  for (let book of arr) {
+    let { title, author, pages, read } = book;
+    addBookToTable(title, author, pages, read);
+  }
+}
+
+addLibrarytoTable(myLibrary);
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  addBookToTable(title, author, pages, read);
+}
