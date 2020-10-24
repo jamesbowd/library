fd9e03c77048eb397bb42e7f042d00f151291803
James Bowden <jamesbowd@gmail.com>
2020-10-24 10:43:53 +0100

    changed to re-build the table each time a book is added. Added ability to delete books

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let myLibrary = [];
 
 const table = document.querySelector("table");
 const form = document.querySelector("form");
+let deleteRows;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -10,8 +11,25 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToTable(title, author, pages, read) {
+const deleteTable = () => {
+  const table = document.querySelector("table");
+
+  table.innerHTML = `
+  <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Pages</th>
+          <th>Read</th>
+          <th>Toggle Read</th>
+          <th>Delete</th>
+        </tr>
+  `;
+};
+
+function addBookToTable(title, author, pages, read, index) {
   const row = document.createElement("tr");
+
+  //row.setAttribute("data-arr-pos", index);
 
   if (read === true) {
     read = "Read";
@@ -25,24 +43,26 @@ function addBookToTable(title, author, pages, read) {
   <td>${pages}</td>
   <td>${read}</td>
   <td>Toggle</td>
-  <td>Trash</td>
+  <td class="delete" data-arr-pos = ${index}>Trash</td>
   `;
   table.appendChild(row);
 }
 
 function addLibrarytoTable(arr) {
+  deleteTable();
   for (let book of arr) {
     let { title, author, pages, read } = book;
-    addBookToTable(title, author, pages, read);
+    const index = myLibrary.indexOf(book);
+    addBookToTable(title, author, pages, read, index);
   }
+  makeDeleteReady();
 }
-
-addLibrarytoTable(myLibrary);
 
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
-  addBookToTable(title, author, pages, read);
+  const index = myLibrary.indexOf(book);
+  addLibrarytoTable(myLibrary);
 }
 
 form.addEventListener("submit", (event) => {
@@ -54,3 +74,32 @@ form.addEventListener("submit", (event) => {
   const readValue = read.checked;
   addBookToLibrary(titleValue, authorValue, pagesValue, readValue);
 });
+
+function getArrPos() {
+  const rows = table.querySelectorAll("tr");
+  for (let row of rows) {
+    const arrPos = row.dataset.arrPos;
+    console.log(arrPos);
+  }
+}
+
+function makeDeleteReady() {
+  const deleteButtonList = document.querySelectorAll(".delete");
+
+  deleteButtonList.forEach((element) => {
+    element.addEventListener("click", function (event) {
+      let keyOfBook = event.target.dataset.arrPos;
+
+      myLibrary.splice(keyOfBook, 1);
+
+      addLibrarytoTable(myLibrary);
+    });
+  });
+}
+
+addBookToLibrary("Lord of the Rings", "J R R Tolkien", 1735, "read");
+addBookToLibrary("Lord of the Flies", "Willam Gibson", 435, "read");
+addBookToLibrary("Lord of the Flies", "Willam Gibson", 435, "read");
+addBookToLibrary("Lord of the Flies", "Willam Gibson", 435, "read");
+addBookToLibrary("Lord of the Flies", "Willam Gibson", 435, "read");
+addBookToLibrary("Lord of the Flies", "Willam Gibson", 435, "read");
